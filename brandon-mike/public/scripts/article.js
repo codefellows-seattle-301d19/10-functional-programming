b183e742762f4a60ecdb3a0d4ff5e3c2cd66abf4
Adutchguy <mdm_8@hotmail.com>
2017-04-25 10:46:42 -0700

    Updated: 'TODO: Chain together a  and a  call to get a rough count of all words in all articles.'

--- brandon-mike/public/scripts/article.js
+++ brandon-mike/public/scripts/article.js
@@ -62,7 +62,11 @@
 
   // TODO: Chain together a `map` and a `reduce` call to get a rough count of all words in all articles.
   Article.numWordsAll = () => {
-    return Article.all.map().reduce()
+    return Article.all.map(function(ele){
+      return ele.body.split(' ').length;
+    }).reduce(function(acc, val){
+      return acc + val;
+    })
   };
 
   // TODO: Chain together a `map` and a `reduce` call to produce an array of unique author names. You will
