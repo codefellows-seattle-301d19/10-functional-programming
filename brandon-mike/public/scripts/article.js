13d7931ccf6ec619d4814d3baeab2622d9c523e3
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2017-04-25 20:57:06 -0700

    completed last two TODOs on article.js

--- brandon-mike/public/scripts/article.js
+++ brandon-mike/public/scripts/article.js
@@ -69,26 +69,45 @@
     })
   };
 
-  // TODO: Chain together a `map` and a `reduce` call to produce an array of unique author names. You will
+  // ***TODO: Chain together a `map` and a `reduce` call to produce an array of unique author names. You will
   // probably need to use the optional accumulator argument in your reduce call.
   Article.allAuthors = () => {
-    return Article.all.map(ele => {
-      return ele.author
-    }).reduce()
+    return Article.all.map(eachArticle => {
+      return eachArticle.author;
+    }).reduce((arrayNames, currentName) => {
+      if (!arrayNames.includes(currentName)) {
+        arrayNames.push(currentName);
+      }
+
+      return arrayNames;
+    }, []);
   };
 
   Article.numWordsByAuthor = () => {
-    return Article.allAuthors().map(author => {
-      // TODO: Transform each author string into an object with properties for
-      // the author's name, as well as the total number of words across all articles
-      // written by the specified author.
-      // HINT: This .map should be setup to return an object literal with two properties.
-      // The first property should be pretty straightforward, but you will need to chain
-      // some combination of filter, map, and reduce to get the value for the second
-      // property.
-
-    })
+    return Article.allAuthors().map(authorName => {
+      return { wordCount: 0, name: authorName };
+    }).map(authorCountObj => {
+      let filteredByAuthor = Article.all.filter((article) => {
+        return article.author === authorCountObj.name;
+      });
+      let authorWordCount = filteredByAuthor.map(oneArticle => {
+        return oneArticle.body.split(' ').length;
+      }).reduce((runningTotal, oneValueInArray) => {
+        return runningTotal + oneValueInArray;
+      }, 0);
+      authorCountObj.wordCount = authorWordCount;
+      return authorCountObj;
+    });
   };
+
+  // ***TODO: Transform each author string into aobject with properties for
+  // the author's name, as well as the total numbeof words across all articles
+  // written by the specified author.
+  // HINT: This .map should be setup to return aobject literal with two properties.
+  // The first property should be prettstraightforward, but you will need to chain
+  // some combination of filter, map, and reduce tget the value for the second
+  // property.
+
 
   Article.truncateTable = callback => {
     $.ajax({
